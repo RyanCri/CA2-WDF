716f81328418ddcc9e8ca3862326d7669d6bf2b7
RyanCri <N00221108@iadt.ie>
2023-12-17 13:06:13 +0000

    finished main design of index

--- js/main.js
+++ js/main.js
@@ -7,3 +7,37 @@ function scrollFunction() {
     document.getElementById("main-nav").style.backgroundColor = "transparent";
   }
 }
+
+// function toggleLocation(){
+//   var location = document.getElementById("location");
+
+//   if (location.style.display === "none") {
+//     location.style.display = "block";
+//   } else {
+//     location.style.display = "none";
+//   }
+
+//   console.log(location.style.display);
+// }
+
+function toggleLocationRath(){
+  document.getElementById("location-rath").classList.toggle("d-none");
+
+  document.getElementById("loc-head-rath").classList.toggle("d-none");
+
+  document.getElementById("location-inch").classList.toggle("d-none");
+
+  document.getElementById("loc-head-inch").classList.toggle("d-none");
+
+  // console.log(document.getElementById("loc-head-inch"));
+}
+
+function toggleLocationInch(){
+  document.getElementById("location-rath").classList.toggle("d-none");
+
+  document.getElementById("loc-head-rath").classList.toggle("d-none");
+
+  document.getElementById("location-inch").classList.toggle("d-none");
+
+  document.getElementById("loc-head-inch").classList.toggle("d-none");
+}
